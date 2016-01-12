ab26166f79c071e616dd4c0e667cb8a0f6bf5b06
LoveUlitka <nastya-rastya@yandex.ru>
2016-01-12 10:38:30 +0300

    fixes
    lowercase fix. path join. warning 32\64-bit. some renaming

--- test_dino.py
+++ test_dino.py
@@ -1,31 +1,36 @@
 import os
-import os.path
 
-ENTER_POINT = 'C:\\Windows\\System32\\'
+ENTER_POINT = 'C:\\Windows\\System32'
+# Warning! when run 32-bit python on 64-bit Windows
+# \System32 path will be swapped to \SysWOW64 internally
+
 FILE_EXT = '.dll'
 DIR_PREFIX = 'a'
-DELIM = '\\'
 
 
-def items_enumeration(start_path, path_to_name, dirs):
+# for given directory (start_path):
+#   append all subdirs into dirs
+#   insert all files into path_to_name dict
+def visit_dir(start_path, path_to_name, dirs):
     items = os.listdir(start_path)
-
     for item in items:
-        cur_path = start_path + DELIM + item
+        cur_path = os.path.join(start_path, item)
         if os.path.isfile(cur_path) and item.lower().endswith(FILE_EXT):
             path_to_name[cur_path] = item
-        elif os.path.isdir(cur_path) and item.lower()[0] == DIR_PREFIX:
+        elif os.path.isdir(cur_path) and item[0] == DIR_PREFIX:
             dirs.append(cur_path)
 
 
-def dirs_enumeration(start_path):
+# recursive dir traverse
+# return dict {full path : filename}
+def dir_iter(start_path):
     path_to_name = {}
-    dirs = []
-    items_enumeration(start_path, path_to_name, dirs)
+    dirs = []  # dirs to proceed
+    visit_dir(start_path, path_to_name, dirs)
 
     while dirs:
         sub_path = dirs.pop()
-        items_enumeration(sub_path, path_to_name, dirs)
+        visit_dir(sub_path, path_to_name, dirs)
     return path_to_name
 
 
@@ -34,7 +39,7 @@ def print_answer(path_to_name):
 
     print('Files:')
     for key, value in path_to_name.items():
-        print(value)
+        print(key + " -> " + value)
 
-path_to_name = dirs_enumeration(ENTER_POINT)
+path_to_name = dir_iter(ENTER_POINT)
 print_answer(path_to_name)
